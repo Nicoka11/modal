d88bbc9c777049271f7ee724533054f829e6b446
Nicolas Ka <n.ka@lepermislibre.fr>
2022-11-01 15:24:52 +0100

    fix: export close button element

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,9 +3,16 @@ import Modal, {
   ModalBackground,
   ModalContainer,
   ModalChildren,
+  CloseButton,
 } from "./Modal";
 import CloseIcon from "./Close";
 
 export default Modal;
-export { ModalBackground, ModalContainer, ModalChildren, CloseIcon };
+export {
+  ModalBackground,
+  ModalContainer,
+  ModalChildren,
+  CloseIcon,
+  CloseButton,
+};
 export type { ModalProps };
